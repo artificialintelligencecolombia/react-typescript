e96340e7e1af2a6bf7b78faae062f45f906c37c2
Daniel Maldonado <damarias991@gmail.com>
2026-01-07 05:15:19 -0500

    feat: todo - check done and delete functionalities enabled

--- src/components/singleTodo.tsx
+++ src/components/singleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import type { Todo } from '../model'
 import {AiFillEdit, AiFillDelete} from 'react-icons/ai'
 import { MdDone } from "react-icons/md"
@@ -11,15 +11,61 @@ type Props = {
 }
 
 const SingleTodo = ({todo, todos, setTodos}: Props) => {
+
+    // 1. Add the states to track if we're editing 
+    const [edit, setEdit] = useState<Boolean>(false); // Are we editing?
+    const [editTodo, setEditTodo] = useState<string>(todo.todo); // What's the new text?
+
+    const handleEdit = (id: number) => {
+        setTodos(
+            todos.map((todo)=>
+                todo.id===id
+            ?{...todo, todo: editTodo}
+            :todo)
+        )
+    }
+
+    const deleteTodo = (id: number) => {
+        setTodos(
+            // Filter out the todo with the matching id
+            todos.filter((todo)=>
+                todo.id !==id 
+            )
+        )
+    };
+
+    // 1. Create the function that receives the todo's id
+    const handleDone = (id: number) => {
+        // 2. Call setTodos to update the entire todos array in parent (App) 
+        setTodos(
+            // 3. Map through ALL todos in the array
+            todos.map((todo)=> 
+                // 4. Check if current todo's id matches the clicked todo's id
+                todo.id===id
+                // 5a. If YES - create new object with isDone toggled
+                ?{...todo,isDone:!todo.isDone}
+                // 5b. If NO - return todo unchanged
+                :todo)
+        )
+    };
+
+
     return (
         <form className="todos__single">
-            <span className="todos__single--text">
-                {todo.todo}
-            </span>
             <div>
-                <span className="icon"><AiFillEdit /></span>
-                <span className="icon"><AiFillDelete /></span>
-                <span className="icon"><MdDone /></span>
+            {
+                // 6. Conditional rendering - if todo is done, show strikethrough
+                todo.isDone ? (
+                    <s className="todos__single--text">{todo.todo}</s>
+                ): (
+                    <span className="todos__single--text">{todo.todo}</span>
+                    )}
+            
+                <span className="icon"><AiFillEdit onClick={()=>handleEdit(todo.id)}/></span>
+            
+                <span className="icon"><AiFillDelete onClick={()=>deleteTodo(todo.id)}/></span>
+                {/* 7: onClick triggers handleDone, passing this todo's id */}
+                <span className="icon"><MdDone onClick={()=>handleDone(todo.id)}/></span>
             </div>
         </form>
     )
